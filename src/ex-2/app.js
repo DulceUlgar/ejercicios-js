9e5bcb64622e179f50d2dfe1cf6d705c0e19cd46
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-05-30 12:32:07 +0200

    realizacion de las actividades

---
+++ src/ex-2/app.js
@@ -0,0 +1,9 @@
+function sumar(){
+    let valor1 = Number(document.getElementById("valor1").value);   
+    let valor2 = Number(document.getElementById("valor2").value);
+
+let result = valor1 + valor2;
+
+document.getElementById("result").value = result;
+
+}
