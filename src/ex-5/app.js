3ddc75b22ab6b650b20f7495d9321ff1f1a2068d
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-29 11:11:59 +0200

    Update app5.js

--- src/ex-5/app.js
+++ src/ex-5/app.js
@@ -1,8 +1,18 @@
-var num1 = prompt("Enter first number");
-var num2 = prompt("Enter second number");
+//Ejercicio de JavaScript 1
 
-if (Number(num1) > Number(num2)) {
-    alert(num1 + "Es el mayor.");
-} else {
-    alert(num2 + "Es el mayor");
+//Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+//El resultado -> el número mayor es
+
+//Hay que vincular el archivo js con el html
+//Hay que vicular el archivo css con el html
+
+// Solicitar al usuario dos números
+const numero1 = prompt("Introduce el primer número: ");
+const numero2 = prompt("Introduce el segundo número: ");
+
+// Comparar los números y mostrar el resultado
+if (numero1 > numero2) {
+  console.log("El primer número, " + numero1 + ", es mayor que el segundo número, " + numero2 + ".");
+} else if (numero2 > numero1) {
+  console.log("El segundo número, " + numero2 + ", es mayor que el primer número, " + numero1 + ".");
 }
