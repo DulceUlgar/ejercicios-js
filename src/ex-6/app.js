92f49a87d7e85a48c36def5650dececd1e47b85a
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-06-29 11:11:16 +0200

    Update app6.js

--- src/ex-6/app.js
+++ src/ex-6/app.js
@@ -1,23 +1,26 @@
-//Definir Valiables
-//Lectura de datos Escribir "Ingresar los 3 números")
-var num1, num2, num3, t, mayor;
+//Ejercicio de JavaScript 1
 
- num1 =parseInt(prompt('Write a first number'));
- num2 =parseInt(prompt('Write a second number'));
- num3 =parseInt(prompt('Write a third number'));
+//Escribe un programa que pida tres números y escriba en la pantalla cual es el mayor.
+//El resultado -> el número mayor es
 
+//Hay que vincular el archivo js con el html
+//Hay que vicular el archivo css con el html
 
+// Solicitar al usuario tres números
+const numero1 = prompt("Ingrese el primer número: ");
+const numero2 = prompt("Ingrese el segundo número: ");
+const numero3 = prompt("Ingrese el tercer número: ");
 
-if(num1 > num2) {
-    t = num1;
-}else{
-    t = num2;
+// Determinar el mayor de los tres números
+let mayor = numero1;
+
+if (numero2 > mayor) {
+  mayor = numero2;
 }
 
-if( t = num3) {
-    mayor = t;
-}else{
-    mayor = num3;
+if (numero3 > mayor) {
+  mayor = numero3;
 }
 
-alert("El mayor es " +mayor);
+// Mostrar el número mayor en pantalla
+console.log("El número mayor de los tres es: " + mayor);
